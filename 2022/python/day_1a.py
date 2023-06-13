bc16ba6d964838d618b30231b49b6eaeb8f837a2
kobejk <105041728+kobejk@users.noreply.github.com>
2023-06-14 09:52:48 +1000

    read file into sublists seperated by blank lines

--- 2022/python/day_1a.py
+++ 2022/python/day_1a.py
@@ -2,14 +2,26 @@
 try:
     file = open("day_1a_input.txt", "rt")
 
-    # first create an empty list
+    # create some empty arrays
     file_list = []
+    current_sublist = []
 
     # add each line of the file to the list
-    for x in file:
-        file_list.append(x)
+    for line in file:
+        # first strip lines of any possible white space
+        line = line.strip()
+        # if the line is not empty, add it to the current sublist
+        if line != "":
+            current_sublist.append(line)
+        # if the line is empty, add sublist to file_list and return empty sublist
+        else:
+            file_list.append(current_sublist)
+            current_sublist = []
 
-    print(file_list[0])
+    # print first two sublists to test
+    print("First file list", file_list[0])
+    print("Second file list", file_list[1])
+    print("Great success!!!")
 
     file.close()
 except Exception:
