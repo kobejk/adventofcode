5020f0209fdfe4e842f8604b523586612dd14ae7
kobejk <105041728+kobejk@users.noreply.github.com>
2023-06-15 13:57:30 +1000

    completed day_1a python

--- 2022/python/day_1a.py
+++ 2022/python/day_1a.py
@@ -12,16 +12,27 @@ try:
         line = line.strip()
         # if the line is not empty, add it to the current sublist
         if line != "":
-            current_sublist.append(line)
+            current_sublist.append(int(line))
         # if the line is empty, add sublist to file_list and return empty sublist
         else:
             file_list.append(current_sublist)
             current_sublist = []
 
-    # print first two sublists to test
-    print("First file list", file_list[0])
-    print("Second file list", file_list[1])
-    print("Great success!!!")
+    most_calories = 0
+    current_calories = 0
+
+    # loop through each array and count calories
+    for sublist in file_list:
+        for calorie_count in sublist:
+            current_calories += calorie_count
+
+        # check if current list is more calories than the largest amount so far
+        if current_calories > most_calories:
+            most_calories = current_calories
+
+        current_calories = 0
+
+    print("The most calories being carried by an elf is:", most_calories)
 
     file.close()
 except Exception:
