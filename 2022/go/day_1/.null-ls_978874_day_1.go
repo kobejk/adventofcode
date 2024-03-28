d4d8b4200634f8036d288c606a55516062c6a35a
kobejk <105041728+kobejk@users.noreply.github.com>
2024-03-29 09:26:58 +1100

    golang day 1

--- 2022/go/day_1/.null-ls_978874_day_1.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import (
-  "fmt"
-  "io"
-  "os"
-  "bufio"
-)
--- 2022/go/day_1/day_1.go
+++ 2022/go/day_1/day_1.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
 	"fmt"
-	// "io"
+	"log"
 	"os"
+	"strconv"
 )
 
 // helper function to check for errors
@@ -15,8 +16,47 @@ func check(e error) {
 }
 
 func main() {
-	dat, err := os.ReadFile("./input.txt")
-	check(err)
-	fmt.Print((string(dat)))
 
+	f, err := os.Open("./input.txt")
+	defer f.Close()
+
+	check(err)
+
+	scanner := bufio.NewScanner(f)
+
+	maxCal := 0
+	currentCal := 0
+	elf := 1
+
+	for scanner.Scan() {
+		// read contents of a single line
+		line := scanner.Text()
+
+		// if line is empty, update max calories
+		if line == "" {
+			if currentCal > maxCal {
+				maxCal = currentCal
+				elf += 1
+			}
+			currentCal = 0
+			// if line is not empty, update current elf total
+		} else {
+			value, err := strconv.Atoi(line)
+			check(err)
+			currentCal += value
+		}
+	}
+
+	// final data block is not followed by a blank line
+	if currentCal > maxCal {
+		maxCal = currentCal
+		elf += 1
+	}
+
+	fmt.Println("Elf", elf, "is carrying", maxCal, "calories.")
+
+	// check for any scanning errors
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
