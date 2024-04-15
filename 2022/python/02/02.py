a6ab0c12acd72d0399ce16d5370d46a308d753fd
kobejk <105041728+kobejk@users.noreply.github.com>
2024-04-15 16:21:03 +1000

    completed python day 02 part a

--- 2022/python/02/02.py
+++ 2022/python/02/02.py
@@ -1,15 +1,19 @@
+# Handle conents possibly being unbound
+contents = ""
+
 # Open file
 try:
     with open("./input_02.txt", "r") as file:
-        # Process each line
-        for line in file:
-            # strip() removes the newline character at the end of the line
-            print(line.strip())
+        contents = file.read()
 except FileNotFoundError:
     print("File not found.")
 
 # Define mappings for strategy guide
-strategy_guide = {"A": "Y", "B": "X", "C": "Z"}
+strategy_guide = {
+    "A": "Y",  # rock (A) vs paper (Y)
+    "B": "X",  # paper (B) vs rock (x)
+    "C": "Z",  # scissors (C) vs scissors (Z)
+}
 
 # Define scores for choice of rock, paper or scissors
 choice_score = {
@@ -20,3 +24,33 @@ choice_score = {
 
 # Define scores for win, lose or draw
 outcome_score = {"win": 6, "draw": 3, "lose": 0}
+
+
+# Play a round
+def determine_outcome(oppenent, response):
+    win_conditions = {("A", "Y"), ("B", "Z"), ("C", "X")}
+    draw_conditions = {("A", "X"), ("B", "Y"), ("C", "Z")}
+    if (oppenent, response) in win_conditions:
+        return "win"
+    elif (oppenent, response) in draw_conditions:
+        return "draw"
+    else:
+        return "lose"
+
+
+total_score = 0
+try:
+    # Split the contens of the file into different rounds by line
+    rounds = contents.strip().split("\n")
+    for round in rounds:
+        moves = round.split()
+        opponent_move = moves[0]
+        your_move = moves[1]
+
+        outcome = determine_outcome(opponent_move, your_move)
+        round_score = choice_score[your_move] + outcome_score[outcome]
+        total_score += round_score
+except Exception as e:
+    print(f"An error occured: {e}")
+
+print("Total score:", total_score)
